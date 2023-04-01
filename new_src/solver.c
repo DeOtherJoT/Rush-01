32a8e010561bf5eb3e46f5a50958cfc8d360f37e
Josh Thor <wunjosh@gmail.com>
2023-04-01 20:13:23 +0800

    feat: Added solver

---
+++ new_src/solver.c
@@ -0,0 +1,143 @@
+#include "../includes/rush2.h"
+
+int	check_double(int *ans, int edge, int place)
+{
+	int	curr_val;
+	int	i;
+
+	curr_val = ans[place];
+	i = -1;
+	while (++i < (place % edge))
+	{
+		if (ans[(edge * (place / edge)) + i] == curr_val)
+			return (FALSE);
+	}
+	i = -1;
+	while (++i < (place / edge))
+	{
+		if (ans[(i * edge) + (place % edge)] == curr_val)
+			return (FALSE);
+	}
+	return (TRUE);
+}
+
+int	check_top_bottom(int *ans, int *clue, int edge, int place)
+{
+	t_cons	dat;
+
+	set_cons_data(&dat, edge);
+	dat.pos_max = get_pos_max(ans, edge, place, 1);
+	while (++dat.ctr1 < dat.pos_max)
+	{
+		if (ans[(dat.ctr1 * edge) + (place % edge)] > dat.max1)
+		{
+			dat.max1 = ans[(dat.ctr1 * edge) + (place % edge)];
+			dat.visible_1++;
+		}
+	}
+	while (--dat.ctr2 > dat.pos_max)
+	{
+		if (ans[(dat.ctr2 * edge) + (place % edge)] > dat.max2)
+		{
+			dat.max2 = ans[(dat.ctr2 * edge) + (place % edge)];
+			dat.visible_2++;
+		}
+	}
+	if (dat.visible_1 != clue[place % edge] ||
+		dat.visible_2 != clue[edge + (place % edge)])
+		return (FALSE);
+	return (TRUE);
+}
+
+int	check_left_right(int *ans, int *clue, int edge, int place)
+{
+	t_cons	dat;
+
+	set_cons_data(&dat, edge);
+	dat.pos_max = get_pos_max(ans, edge, place, 2);
+	while (++dat.ctr1 < dat.pos_max)
+	{
+		if (ans[(edge * (place / edge)) + dat.ctr1] > dat.max1)
+		{
+			dat.max1 = ans[(edge * (place / edge) + dat.ctr1)];
+			dat.visible_1++;
+		}
+	}
+	while (--dat.ctr2 > dat.pos_max)
+	{
+		if (ans[(edge * (place / edge)) + dat.ctr2] > dat.max2)
+		{
+			dat.max2 = ans[(edge * (place / edge)) + dat.ctr2];
+			dat.visible_2++;
+		}
+	}
+	if (dat.visible_1 != clue[(edge * 2) + (place / edge)] ||
+		dat.visible_2 != clue[(edge * 3) + (place / edge)])
+		return (FALSE);
+	return (TRUE);
+}
+
+/**
+ * @brief	Checks if the value at a certain place fits within the constraints
+ * 			which are no repetitions and with respect to the edge clues.
+ * 
+ * @param ans 
+ * @param clue 
+ * @param edge 
+ * @param place 
+ * @return int 
+ */
+
+int	constraint_check(int *ans, int *clue, int edge, int place)
+{
+	if (check_double(ans, edge, place) == FALSE)
+		return (FALSE);
+	if ((place / edge) == (edge - 1))
+	{
+		if (check_top_bottom(ans, clue, edge, place) == FALSE)
+			return (FALSE);
+	}
+	if ((place % edge) == (edge - 1))
+	{
+		if (check_left_right(ans, clue, edge, place) == FALSE)
+			return (FALSE);
+	}
+	return (TRUE);
+
+}
+
+/**
+ * @brief	Backtracking algorithm to solve the skyscrapers puzzle.
+ * 			Target to solve 7x7 in a reasonable time.
+ * 
+ * @param data		the struct that holds all necessary data
+ * @param place 	the current place in the board where the possible
+ * 					values are being placed.
+ * @return int 	Return TRUE if answer is found, FALSE if no answer is found
+ * 				that fulfills the constraints at the current location.
+ */
+
+int	solver(t_data *data, int place)
+{
+	int	value;
+
+	/* Base Case, a solution is found. */
+	if (place == (data->edge * data->edge))
+		return (TRUE);
+	value = 1;
+	while (value <= data->edge)
+	{
+		data->answer[place] = value;
+		if (constraint_check(data->answer, data->clues, data->edge, place))
+		{
+			/* General Case, place is incremented as recurse control */
+			if (solver(data, place + 1) == TRUE)
+				return (TRUE);
+		}
+		else
+			data->answer[place] = 0;
+		value++;
+	}
+	/* If no value is possible, a previous place has a wrong value.*/
+	return (FALSE);
+}
---
+++ new_src/utils_solver.c
@@ -0,0 +1,35 @@
+#include "../includes/rush2.h"
+
+int	get_pos_max(int *answer, int edge, int place, int flag)
+{
+	int	i;
+
+	i = -1;
+	if (flag == 1) // check top bottom
+	{
+		while (++i < edge)
+		{
+			if (answer[(i * edge) + (place % edge)] == edge)
+				return (i);
+		}
+	}
+	else // check left right
+	{
+		while (++i < edge)
+		{
+			if (answer[(edge * (place / edge)) + i] == edge)
+				return (i);
+		}
+	}
+	return (-1);
+}
+
+void	set_cons_data(t_cons *dat, int edge)
+{
+	dat->ctr1 = -1;
+	dat->ctr2 = edge;
+	dat->visible_1 = 1;
+	dat->visible_2 = 1;
+	dat->max1 = 0;
+	dat->max2 = 0;
+}
