7c52c850dfe99356b10638a513b768abb9c711c0
Josh Thor <wunjosh@gmail.com>
2023-04-01 20:10:17 +0800

    feat: Added new struct for constraints
    feat: Added solver and other util functs

--- includes/rush2.h
+++ includes/rush2.h
@@ -8,12 +8,23 @@
 # define FALSE 0
 # define TRUE 1
 
-typedef struct s_data
+typedef struct	s_data
 {
 	int	*answer;
 	int	*clues;
 	int	edge;
 }	t_data;
+
+typedef struct	s_cons
+{
+	int	ctr1;
+	int	ctr2;
+	int	visible_1;
+	int	visible_2;
+	int max1;
+	int	max2;
+	int pos_max;
+}	t_cons;
 
 // ft_split.c
 char	**ft_split(char *str);
@@ -24,9 +35,15 @@ t_data	*parse_input(char *clue_str);
 // main.c
 int		ft_error(char *str, int ret);
 
+// solver.c
+int		solver(t_data *data, int place);
+
+// utils_solver.c
+int		get_pos_max(int *answer, int edge, int place, int flag);
+void	set_cons_data(t_cons *dat, int edge);
+
 // utils.c
 size_t	ft_strlen(char *str);
-void	ft_free_arr(char **arr);
 int		ft_atoi(char *str);
 int		ft_sqrt(int nb);
 void	ft_free_data(t_data *data);
@@ -35,6 +52,6 @@ void	ft_free_data(t_data *data);
 int		ft_error(char *str, int ret);
 void	ft_putchar(char c);
 void	ft_putnbr(int nb);
-
+void	ft_free_split(char **arr);
 
 #endif
